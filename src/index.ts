7293ec93605deed766feae60ebab9c4f98a46481
BlancoAg <agustinbla12@gmail.com>
2023-02-10 18:10:35 -0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,13 @@
-import cron from 'node-cron';
+const cron = require('node-cron');
+const axios = require('axios');
 
 cron.schedule(`*/1 * * * *`, async () => {
-  console.log(`running your task...`);
+  console.log(`Fetching website "https://dinosaurweb.up.railway.app/"...`);
+  try {
+    const response = await axios.get("https://dinosaurweb.up.railway.app/");
+    console.log(`Website fetched successfully with status code: ${response.status}`);
+  } catch (error) {
+    console.error(`Error fetching website: ${error}`);
+  }
 });
+
